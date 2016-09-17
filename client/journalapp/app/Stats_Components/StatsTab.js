070dfaca79dc2523087bd5ade93afa1d74710476
Teague Ashburn <teague@ashburn.digital>
2016-09-16 19:50:10 -0700

    Updates to styling and content of StatsTab

--- client/journalapp/app/Stats_Components/StatsTab.js
+++ client/journalapp/app/Stats_Components/StatsTab.js
@@ -33,11 +33,7 @@ export default class StatsTab extends Component {
   render() {
     let msg7day;
     let last7dayPercent = this.props.entriesLast7Days / 7;
-<<<<<<< 7bbb342d8bb45259d8fabbe1384a382d727d3c06
 
-=======
-    
->>>>>>> Progress on stats view, including dummy data for testing
     if (last7dayPercent === 0) {
       msg7day = "That's a big goose egg, buster!";
     } else if (last7dayPercent * 100 < 50) {
