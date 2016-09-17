3859243bce88c574473aca4c36c5e5ecfb58e7af
Teague Ashburn <teague@ashburn.digital>
2016-09-16 19:50:10 -0700

    Updates to styling and content of StatsTab

--- client/journalapp/app/styles/StatsTabStyles.js
+++ client/journalapp/app/styles/StatsTabStyles.js
@@ -21,10 +21,58 @@ const styles = StyleSheet.create({
     position:'relative',
     fontSize: 14,
     fontWeight: '400',
-    color:"#666666",
-    alignSelf:'center'
+    color:"black",
+    alignSelf:'flex-start',
+    marginLeft: 10,
+    marginRight: 10
   },
-  header: {}
+  bodyText2: {
+    flexDirection: 'column',
+    position:'relative',
+    fontSize: 14,
+    fontWeight: '400',
+    color:"black",
+    alignSelf:'flex-start',
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 10
+  },
+  statCatBox1: {
+    backgroundColor: '#aed6f1',
+    paddingBottom: 10,
+    paddingTop: 10
+  },
+  statCatBox2: {
+    backgroundColor: '#d5f5e3',
+    paddingBottom: 10,
+    paddingTop: 10
+  },
+  statCatBox3: {
+    backgroundColor: '#f9e79f',
+    paddingBottom: 10,
+    paddingTop: 10
+  },
+  statCatBox4: {
+    backgroundColor: '#f1948a',
+    paddingBottom: 10,
+    paddingTop: 10
+  },
+  mainHead: {
+    alignSelf: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    flexDirection: 'row',
+    marginBottom: 10
+  },
+  categoryHead: {
+    flex: .2,
+    marginTop: 5,
+    color: 'red'
+  },
+  numView: {
+    fontSize: 55,
+    marginLeft: 10
+  }
 });
 
 module.exports = styles;
